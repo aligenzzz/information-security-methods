37b345115a403c9fe7f916720ac3e82ad39e6e2d
aligenzzz <alina_bybko@mail.ru>
2024-09-26 14:20:44 +0300

    fix: lab 3

--- lab3/main.py
+++ lab3/main.py
@@ -2,17 +2,20 @@ from rabin import Rabin
 
 
 if __name__ == '__main__':
-    input_filename = input('Input file: ')
-    output_filename = input('Output file: ')
-    mode = input('Mode? (e/d) ')
+    input_filename = 'input.txt'
+    output_filename = 'encrypted.txt'
+    decrypted_filename = 'decrypted.txt'
 
-    rabin = Rabin(512)
-    rabin.generate_keys()
-
-    if mode == 'e':
-        rabin.encrypt_file(input_filename, output_filename)
-    elif mode == 'd':
-        rabin.decrypt_file(input_filename, output_filename)
-    else:
-        raise Exception('Incorrect mode!')
+    rabin = Rabin(512, 6, 50)
     
+    while True:
+        try:
+            rabin.generate_keys()
+
+            rabin.encrypt_file(input_filename, output_filename)
+            rabin.decrypt_file(output_filename, decrypted_filename)
+
+            break
+        except Exception as e:
+            pass
+        
--- lab3/rabin.py
+++ lab3/rabin.py
@@ -6,9 +6,14 @@ class Rabin:
     bits = 0
     public_key = 0
     private_key = 0
+    threshold = 0
+    chunk_size = 0
+    __encrypted_chunks = []
 
-    def __init__(self, bits: int) -> None:
+    def __init__(self, bits: int, threshold: int, chunk_size: int) -> None:
         self.bits = bits
+        self.threshold = threshold
+        self.chunk_size = chunk_size
 
     # p ≡ q ≡ 3 mod 4
     def generate_prime_number(self) -> int:
@@ -28,9 +33,9 @@ class Rabin:
         self.private_key = (p, q)
 
     def convert_message_to_int(self, message : str) -> int:
-        converted = Crypto.Util.number.bytes_to_long(message.encode('utf-8'))
+        converted = Crypto.Util.number.bytes_to_long(message.encode('utf-16'))
         bit_string = bin(converted)
-        output = bit_string + bit_string[-6:]
+        output = bit_string + bit_string[-self.threshold:]
         int_output = int(output, 2)
         return int_output
 
@@ -50,16 +55,16 @@ class Rabin:
     def select_solution(self, solutions: list[int]) -> int:
         for i in solutions:
             binary = bin(i)
-            append = binary[-6:]
-            binary = binary[:-6]
+            append = binary[-self.threshold:]
+            binary = binary[:-self.threshold]
 
-            if append == binary[-6:]:
+            if append == binary[-self.threshold:]:
                 return i     
         return
     
     def convert_int_to_message(self, number: int) -> str:
-        formatted_text = format(number, 'x')
-        decrypted_text = bytes.fromhex(formatted_text).decode()
+        formatted_text = format(number, 'x')        
+        decrypted_text = bytes.fromhex(formatted_text).decode('utf-16', errors='ignore')
         return decrypted_text
 
     def decrypt(self, cipher: int) -> str:
@@ -87,19 +92,43 @@ class Rabin:
         return decrypted_text
 
     def encrypt_file(self, input_file: str, output_file: str) -> None:
-        with open(input_file, 'r') as file:
-            input_text = file.read()
+        with open(input_file, 'rb') as file:
+            input_bytes = file.read()
+        input_text = input_bytes.decode('utf-16', errors='ignore') 
+            
+        chunks = [input_text[i:i + self.chunk_size] for i in range(0, len(input_text), self.chunk_size)]
 
-        encrypted_text = self.encrypt(input_text)
-
-        with open(output_file, 'w') as file:
-            file.write(encrypted_text)
+        encrypted_chunks = []
+        for chunk in chunks:
+            encrypted_text = self.encrypt(chunk) 
+            encrypted_chunks.append(encrypted_text)
+        
+        with open(output_file, 'w', encoding='utf-16') as file:
+            for encrypted_chunk in encrypted_chunks:
+                file.write(encrypted_chunk)
+        
+        self.__encrypted_chunks = encrypted_chunks
+        
+    def delete_empty_lines(self, filename: str) -> None:
+        with open(filename, 'r', encoding='utf-16') as file:
+            lines = file.readlines()
+            
+        non_empty_lines = [line for line in lines if line.strip()]
+        
+        with open(filename, 'w', encoding='utf-16') as file:
+            file.writelines(non_empty_lines)
             
     def decrypt_file(self, input_file: str, output_file: str) -> None:
-        with open(input_file, 'r') as file:
+        with open(input_file, 'r', encoding='utf-16') as file:
             input_text = file.read()
 
-        decrypted_text = self.decrypt(int(input_text))
-        
-        with open(output_file, 'w') as file:
-            file.write(decrypted_text)
+        decrypted_chunks = []
+        for chunk in self.__encrypted_chunks:
+            decrypted_text = self.decrypt(int(chunk))
+            decrypted_chunks.append(decrypted_text)
+                    
+        with open(output_file, 'w', encoding='utf-16') as file:
+            for decrypted_chunk in decrypted_chunks:
+                file.write(decrypted_chunk)
+                
+        self.delete_empty_lines(output_file)
